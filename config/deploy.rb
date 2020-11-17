e60472038c85da1d4ae882efdebd097bde57ae71
vagrant <vagrant@localhost.localdomain>
2020-11-17 19:47:07 +0900

    changed_deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :rbenv_ruby, '2.5.7'
 set :linked_files, %w{config/master.key .env}
 append :linked_dirs, "log", "public", "tmp"
 
-# Default branch is :master
+# Default branch is :base1
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
